9e3dfacf1eb4c3dbabee1ee1455974467514d604
Bram <39883637+Roboolet@users.noreply.github.com>
2022-04-02 09:14:37 +0200

    Player inputs and movement
    + Added the unity input system package

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class Player : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] float speed;
+    [SerializeField] float speedMax;
+    [SerializeField] float friction;
+    //[Header("References")]
+
+    Vector2 move, moveRaw;
+    float moveRawMagnitude;
+
+    public void Input_Move(CallbackContext input) { moveRaw = input.ReadValue<Vector2>(); }
+
+    public void Input_Interact(CallbackContext input) { }
+
+    private void Update()
+    {
+        float currentMaxSpeed = speedMax * moveRawMagnitude;
+        if (moveRaw != Vector2.zero)
+            move = Vector2.ClampMagnitude(move + moveRaw * speed * Time.deltaTime, currentMaxSpeed);
+        else
+            move = Vector2.Lerp(move, Vector2.zero, friction * Time.deltaTime);
+
+        moveRawMagnitude = moveRaw.magnitude;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(move);
+    }
+}
