295d251327fa1e4b6697faef39cf6cb240a56b93
Bram <39883637+Roboolet@users.noreply.github.com>
2022-04-02 23:11:40 +0200

    RoofedArea

---
+++ Assets/Scripts/RoofedAreaTrigger.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RoofedAreaTrigger : MonoBehaviour
+{
+    [SerializeField] float colorLerp;
+    [SerializeField] UnityEvent OnEntry, OnExit;
+    bool playerIsInside;
+    Camera mainCam;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;   
+    }
+
+    private void LateUpdate()
+    {
+        if (playerIsInside)
+        {
+            mainCam.backgroundColor = Color.Lerp(mainCam.backgroundColor, Color.black, colorLerp);
+        }
+        else
+        {
+            mainCam.backgroundColor = Color.Lerp(mainCam.backgroundColor, Color.white, colorLerp);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerIsInside = true;
+            OnEntry.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerIsInside = false;
+            OnExit.Invoke();
+        }
+    }
+}
