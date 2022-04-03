d3af40c304343c3b4a7c251e8f5b5abc969be91a
Bram <39883637+Roboolet@users.noreply.github.com>
2022-04-03 16:33:57 +0200

    Tree felling animation and drops

--- Assets/Scripts/Interactable/Tree.cs
+++ Assets/Scripts/Interactable/Tree.cs
@@ -9,6 +9,10 @@ public class Tree : Interactable
     [SerializeField] Sprite choppedSprite;
     [SerializeField] float chopsLeft;
 
+    [Space]
+    [SerializeField] Item[] possibleDrops;
+    [SerializeField] int numDrops;
+
 
     protected override void OnInsert(Item item)
     {
@@ -19,11 +23,16 @@ public class Tree : Interactable
         {
             spriteRenderer.sprite = choppedSprite;
             stopAllInteractions = true;
+            GetComponent<Animator>().SetTrigger("Chopped");
         }
     }
 
     public void Anim_Fall()
     {
-
+        for (int i = 0; i < numDrops; i++)
+        {
+            Vector2 pos = new Vector2(transform.position.x + Random.Range(-3f, -1), transform.position.y + Random.Range(-0.4f, 0.4f));
+            ItemDropper.current.DropItem(possibleDrops[Random.Range(0, possibleDrops.Length)], pos);
+        }
     }
 }
